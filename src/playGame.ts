32040b7b2eb2184e91a6ffe81632c84108f63764
Abdi Urgessa <abdiurgessa9@gmail.com>
2024-06-23 15:21:35 +0300

    async added to play games

--- src/playGame.ts
+++ src/playGame.ts
@@ -2,14 +2,14 @@ import { Context } from "telegraf";
 import { sendGameChewata, sendGameLevelUp } from "./keyboards";
 
 
-const playGame = (ctx: Context) => {
+const playGame = async (ctx: Context) => {
     //dev
     // ctx.replyWithGame("chewata", sendGameLevelUp)
     // ctx.replyWithGame("chewata", sendGameChewata)
 
     //prod
-    ctx.replyWithGame("levelup", sendGameLevelUp)
-    ctx.replyWithGame("korki", sendGameChewata)
+    await ctx.replyWithGame("levelup", sendGameLevelUp)
+    await ctx.replyWithGame("korki", sendGameChewata)
 }
 
 
