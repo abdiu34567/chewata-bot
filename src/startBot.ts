ad2e907ee8033dd9abc331d038d9e719881fb67a
Abdi Urgessa <abdiurgessa9@gmail.com>
2024-07-12 11:46:46 +0300

    poll link have added

--- src/startBot.ts
+++ src/startBot.ts
@@ -1,5 +1,5 @@
 import { Context } from "telegraf";
-import { mainMenu, shareContact } from "./keyboards";
+import { mainMenu, Ques1, shareContact } from "./keyboards";
 import { connectToServer, getDb } from "./db/config";
 import { UserController } from "./db";
 import recordDataToSheet, { UserData } from "./Api/sheetApiConfig";
@@ -13,6 +13,13 @@ export const startBot = async (ctx: Context) => {
 
   const message = ctx.message as { text: string };
   const inviter = message?.text?.split(" ")[1];
+
+  if (inviter && inviter.includes("poll")) {
+    return await ctx.replyWithHTML(
+      "<b>Who do you think will win?</b>\n\n (A) <i>Spain</i> \n (B) <i>England</i>",
+      Ques1
+    );
+  }
   // const inviterId = Number(inviter.trim())
 
   var user = await userController.queryUser({ tgId: String(ctx.chat?.id) });
